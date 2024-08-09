aa6492c3d9027a60eebbe321310807672e2c9dbd
mhgholamrezaei <mgh27.1997@gmail.com>
2024-08-08 22:10:35 -0400

    Update AES slurm

---
+++ PIMbench/cpp-aes/execute_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -x
+
+config_files=(
+  "../../configs/iiswc/PIMeval_Bank_Rank16.cfg"
+  "../../configs/iiswc/PIMeval_Bank_Rank1.cfg"
+  "../../configs/iiswc/PIMeval_Bank_Rank32.cfg"
+  "../../configs/iiswc/PIMeval_Bank_Rank4.cfg"
+  "../../configs/iiswc/PIMeval_Bank_Rank8.cfg"
+  "../../configs/iiswc/PIMeval_BitSerial_Rank16.cfg"
+  "../../configs/iiswc/PIMeval_BitSerial_Rank1.cfg"
+  "../../configs/iiswc/PIMeval_BitSerial_Rank32.cfg"
+  "../../configs/iiswc/PIMeval_BitSerial_Rank4.cfg"
+  "../../configs/iiswc/PIMeval_BitSerial_Rank8.cfg"
+  "../../configs/iiswc/PIMeval_Fulcrum_Rank16.cfg"
+  "../../configs/iiswc/PIMeval_Fulcrum_Rank1.cfg"
+  "../../configs/iiswc/PIMeval_Fulcrum_Rank32.cfg"
+  "../../configs/iiswc/PIMeval_Fulcrum_Rank4.cfg"
+  "../../configs/iiswc/PIMeval_Fulcrum_Rank8.cfg"
+)
+
+for config in "${config_files[@]}"
+do
+  # Call the Slurm script for each config file, running each in the background
+  sbatch run_aes.sh $config &
+done
+
+# Wait for all background jobs to finish
+wait
+
---
+++ PIMbench/cpp-aes/run_aes.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name="aes"
+#SBATCH -n 1
+#SBATCH -t 3-00:00:00
+#SBATCH -p cpu
+#SBATCH --cpus-per-task=60
+#SBATCH --mem=128GB
+
+# input size = 16 GB
+input_size=$((2**34))
+
+# Get the config file path from the first argument
+config_file=$1
+
+# Extract the base name of the config file (without path and extension)
+config_name=$(basename "$config_file" .cfg)
+
+# Set the output and error log file names based on the config file name
+error_file="aes-${config_name}.err"
+output_file="aes-${config_name}.out"
+
+# Redirect stdout and stderr to the log files
+exec > "$output_file" 2> "$error_file"
+
+# Print Slurm job information
+echo "Slurm Job ID: $SLURM_JOB_ID"
+echo "Slurm Job Name: $SLURM_JOB_NAME"
+echo "Node List: $SLURM_JOB_NODELIST"
+echo "Number of Nodes: $SLURM_JOB_NUM_NODES"
+echo "CPU Tasks per Node: $SLURM_CPUS_ON_NODE"
+
+# Print the config file being used
+echo "Using config file: $config_file"
+
+# Run the aes.out program with the specified config file
+./PIM/aes.out -f $config_file -v false -l $input_size
+
+
